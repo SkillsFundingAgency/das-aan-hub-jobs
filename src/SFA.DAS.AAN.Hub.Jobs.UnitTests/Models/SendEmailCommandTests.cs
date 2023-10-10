6190eb26bb550f060aeb658896f04a570c9828c7
Deven Shah <devenshah1974@gmail.com>
2023-10-10 21:53:53 +0100

    CSP-975 Added SendEmailCommand model

---
+++ src/SFA.DAS.AAN.Hub.Jobs.UnitTests/Models/SendEmailCommandTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+using AutoFixture;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using SFA.DAS.AAN.Hub.Data.Entities;
+using SFA.DAS.AAN.Hub.Jobs.Models;
+
+namespace SFA.DAS.AAN.Hub.Jobs.UnitTests.Models;
+
+public class SendEmailCommandTests
+{
+    public record TestEmailTemplate(string Name, string Id);
+
+    private TestEmailTemplate _template = null!;
+    private Notification _notification = null!;
+    private SendEmailCommand _sut = null!;
+
+    [SetUp]
+    public void Init()
+    {
+        Fixture fixture = new();
+        _template = fixture.Create<TestEmailTemplate>();
+        _notification = fixture.Build<Notification>().With(n => n.Tokens, JsonSerializer.Serialize(_template)).Create();
+        _sut = new SendEmailCommand(_notification);
+    }
+
+    [Test]
+    public void ThenPopulatesRecipientAddressFromNotificationMemberEmail() =>
+        _sut.RecipientsAddress.Should().Be(_notification.Member.Email);
+
+    [Test]
+    public void ThenPopulatesTemplateIdFromNotificationTemplateName() =>
+        _sut.TemplateId.Should().Be(_notification.TemplateName);
+
+    [Test]
+    public void ThenPopulatesTokensFromNotificationTokens()
+    {
+        using AssertionScope scope = new();
+        _sut.Tokens.Should().HaveCount(2);
+        _sut.Tokens[nameof(TestEmailTemplate.Id)].Should().Be(_template.Id);
+        _sut.Tokens[nameof(TestEmailTemplate.Name)].Should().Be(_template.Name);
+    }
+}
---
+++ src/SFA.DAS.AAN.Hub.Jobs/Models/SendEmailCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using SFA.DAS.AAN.Hub.Data.Entities;
+
+namespace SFA.DAS.AAN.Hub.Jobs.Models;
+
+public class SendEmailCommand
+{
+    public string TemplateId { get; }
+    public string RecipientsAddress { get; }
+    public IReadOnlyDictionary<string, string> Tokens { get; }
+
+    public SendEmailCommand(Notification notification)
+    {
+        TemplateId = notification.TemplateName;
+        RecipientsAddress = notification.Member.Email;
+        Tokens = JsonSerializer.Deserialize<Dictionary<string, string>>(notification.Tokens);
+    }
+}
