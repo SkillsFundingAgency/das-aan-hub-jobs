40c338d78d9ad216fcf43cf8faee7f4a00b1bcf4
Deven Shah <devenshah1974@gmail.com>
2023-10-20 17:13:03 +0100

    CSP-975 Enable MI connection to nservicebus

--- src/SFA.DAS.AAN.Hub.Jobs/Extensions/AddNServiceBusExtension.cs
+++ src/SFA.DAS.AAN.Hub.Jobs/Extensions/AddNServiceBusExtension.cs
@@ -5,7 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using NServiceBus;
 using SFA.DAS.AAN.Hub.Jobs.Configuration;
 using SFA.DAS.Notifications.Messages.Commands;
-using SFA.DAS.NServiceBus.Extensions;
+using SFA.DAS.NServiceBus.Configuration;
+using SFA.DAS.NServiceBus.Configuration.NewtonsoftJsonSerializer;
 
 namespace SFA.DAS.AAN.Hub.Jobs.Extensions;
 
@@ -21,27 +22,47 @@ internal static class AddNServiceBusExtension
         NServiceBusConfiguration nServiceBusConfiguration = new();
         configuration.GetSection(nameof(NServiceBusConfiguration)).Bind(nServiceBusConfiguration);
 
-        var endpointConfiguration = new EndpointConfiguration(EndpointName);
-        endpointConfiguration.EnableInstallers();
-        endpointConfiguration.UseMessageConventions();
-        endpointConfiguration.UseNewtonsoftJsonSerializer();
-        endpointConfiguration.License(nServiceBusConfiguration.NServiceBusLicense);
+        var endpointConfiguration = new EndpointConfiguration(EndpointName)
+                .UseErrorQueue($"{EndpointName}-errors")
+                .UseInstallers()
+                .UseMessageConventions()
+                .UseNewtonsoftJsonSerializer();
+
+        if (!string.IsNullOrEmpty(nServiceBusConfiguration.NServiceBusLicense))
+        {
+            endpointConfiguration.UseLicense(nServiceBusConfiguration.NServiceBusLicense);
+        }
+
         endpointConfiguration.SendOnly();
 
+#if DEBUG
         var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
-        transport.AddRouting(routeSettings =>
-        {
-            routeSettings.RouteToEndpoint(typeof(SendEmailCommand), NotificationsQueue);
-        });
+        transport.Routing().RouteToEndpoint(typeof(SendEmailCommand), RoutingSettingsExtensions.NotificationsMessageHandler);
         var connectionString = nServiceBusConfiguration.NServiceBusConnectionString;
         transport.ConnectionString(connectionString);
+#else
 
+        endpointConfiguration.UseAzureServiceBusTransport(nServiceBusConfiguration.NServiceBusConnectionString, s => s.AddRouting());
+#endif
         var endpointInstance = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
 
         builder.Services
             .AddSingleton(p => endpointInstance)
             .AddSingleton<IMessageSession>(p => p.GetService<IEndpointInstance>());
+#if !DEBUG
+        builder.Services.AddHostedService<NServiceBusHostedService>();
+#endif
 
         return builder;
+    }
+}
+
+public static class RoutingSettingsExtensions
+{
+    public const string NotificationsMessageHandler = "SFA.DAS.Notifications.MessageHandlers";
+
+    public static void AddRouting(this RoutingSettings routingSettings)
+    {
+        routingSettings.RouteToEndpoint(typeof(SendEmailCommand), NotificationsMessageHandler);
     }
 }
