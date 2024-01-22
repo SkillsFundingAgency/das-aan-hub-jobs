bef19871b23474f8c733c96add1c0e8b80104a94
Peter Williams <peter.david.williams.1991@gmail.com>
2024-01-22 16:22:59 +0000

    CSP-1203 - Added more null negation to clear warnings

--- src/SFA.DAS.AAN.Hub.Data/Repositories/MemberDataCleanupRepository.cs
+++ src/SFA.DAS.AAN.Hub.Data/Repositories/MemberDataCleanupRepository.cs
@@ -34,7 +34,7 @@ public class MemberDataCleanupRepository : IMemberDataCleanupRepository
         var query = _context.Members
             .Where(m => m.Status == MemberStatus.Withdrawn.ToString() || m.Status == MemberStatus.Deleted.ToString()
                 && m.Email != m.Id.ToString()
-                && m.EndDate.Value.Day < DateTime.Today.AddDays(-14).Day).
+                && m.EndDate!.Value.Day < DateTime.Today.AddDays(-14).Day).
             Include(m => m.MemberPreferences)
             .Include(m => m.MemberProfiles)
             .Include(m => m.Notifications)
