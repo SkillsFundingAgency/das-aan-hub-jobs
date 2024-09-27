1ba271b467d430193d715f089cd5bbca8d6ac76f
ratheeshri1984 <ratheeshri1984@gmail.com>
2024-09-27 17:46:45 +0100

    EC-622-Admin event attendance cancel email

---
+++ src/SFA.DAS.AAN.Hub.Jobs/Constant/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SFA.DAS.AAN.Hub.Jobs.Constant;
+
+[ExcludeFromCodeCoverage]
+public static class Constants
+{
+    public static class EmailTemplateName
+    {
+        public const string AdminEventAttendanceCancelTemplate = "AANAdminEventAttendanceCancel";
+    }
+}
