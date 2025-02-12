9f62478fa0d15857196ee25ef075cf35a8a5838a
Chris Foster <chrisfoster186@googlemail.com>
2025-02-12 11:30:30 +0000

    Comments some code to isolate bug

--- src/SFA.DAS.AAN.Hub.Jobs/HttpClientConfiguration/HttpClientConfigurationExtension.cs
+++ src/SFA.DAS.AAN.Hub.Jobs/HttpClientConfiguration/HttpClientConfigurationExtension.cs
@@ -50,18 +50,18 @@ namespace SFA.DAS.AAN.Hub.Jobs.HttpClientConfiguration
 
         private static void AddApprenticeOuterApiClient(IServiceCollection services, IConfiguration configuration)
         {
-            var outerApiConfig = configuration.GetSection(nameof(ApplicationConfiguration)).Get<ApplicationConfiguration>().ApprenticeAanOuterApiConfiguration;
+            //var outerApiConfig = configuration.GetSection(nameof(ApplicationConfiguration)).Get<ApplicationConfiguration>().ApprenticeAanOuterApiConfiguration;
 
-            services.AddTransient<IApimClientConfiguration>((_) => outerApiConfig);
+            //services.AddTransient<IApimClientConfiguration>((_) => outerApiConfig);
 
-            services.AddScoped<Http.MessageHandlers.DefaultHeadersHandler>();
-            services.AddScoped<Http.MessageHandlers.LoggingMessageHandler>();
-            services.AddScoped<Http.MessageHandlers.ApimHeadersHandler>();
+            //services.AddScoped<Http.MessageHandlers.DefaultHeadersHandler>();
+            //services.AddScoped<Http.MessageHandlers.LoggingMessageHandler>();
+            //services.AddScoped<Http.MessageHandlers.ApimHeadersHandler>();
 
-            services.AddRestEaseClient<IApprenticeOuterApiClient>(outerApiConfig.ApiBaseUrl)
-                .AddHttpMessageHandler<Http.MessageHandlers.DefaultHeadersHandler>()
-                .AddHttpMessageHandler<Http.MessageHandlers.ApimHeadersHandler>()
-                .AddHttpMessageHandler<Http.MessageHandlers.LoggingMessageHandler>();
+            //services.AddRestEaseClient<IApprenticeOuterApiClient>(outerApiConfig.ApiBaseUrl)
+            //    .AddHttpMessageHandler<Http.MessageHandlers.DefaultHeadersHandler>()
+            //    .AddHttpMessageHandler<Http.MessageHandlers.ApimHeadersHandler>()
+            //    .AddHttpMessageHandler<Http.MessageHandlers.LoggingMessageHandler>();
         }
     }
 }
